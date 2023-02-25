7309b22d264175c464d783bd65f8ec6e56572006
Hannu Lyytinen <hannux@ssrc.tii.ae>
2023-02-25 09:41:45 +0200

    upstream: Support multiple memory areas
    Signed-off-by: Hannu Lyytinen <hannux@ssrc.tii.ae>

--- components/VM_Arm/src/fdt_manipulation.c
+++ components/VM_Arm/src/fdt_manipulation.c
@@ -15,7 +15,10 @@ int fdt_generate_memory_node(void *fdt, uintptr_t base, size_t size)
     int address_cells = fdt_address_cells(fdt, root_offset);
     int size_cells = fdt_size_cells(fdt, root_offset);
 
-    int this = fdt_add_subnode(fdt, root_offset, "memory");
+    char name[64];
+    sprintf(name, "memory@%"PRIxPTR, base);
+
+    int this = fdt_add_subnode(fdt, root_offset, name);
     if (this < 0) {
         return this;
     }
