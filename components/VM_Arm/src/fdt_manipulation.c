6cf8e9ac129a2e83bccc0adf58622497626f55e9
Markku Ahvenjärvi <markkux@ssrc.tii.ae>
2023-11-10 15:04:01 +0200

    Fix use-after-free
    Signed-off-by: Markku Ahvenjärvi <markkux@ssrc.tii.ae>

--- components/VM_Arm/src/fdt_manipulation.c
+++ components/VM_Arm/src/fdt_manipulation.c
@@ -58,8 +58,8 @@ int fdt_generate_chosen_node(void *fdt, const char *stdout_path, const char *boo
     }
 
     /* TODO: fix this hack */
+    char virtio_tmp[1024];
     if (append_vm_virtio_device_cmdline) {
-        char virtio_tmp[1024];
         strcpy(virtio_tmp, bootargs);
         bootargs = append_vm_virtio_device_cmdline(virtio_tmp);
     }
