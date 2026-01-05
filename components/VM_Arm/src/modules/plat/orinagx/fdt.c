2e430a33b446cab1c718a9fd3814dd76d92233c8
Hannu Lyytinen <hannu.lyytinen@unikie.com>
2026-01-05 16:57:30 +0200

    Revert "orinagx: Add MGBE Ethernet FDT customization"
    This reverts commit f656b9a8ef615f03bbfc88e3850bddc1bc013522.

--- components/VM_Arm/src/modules/plat/orinagx/fdt.c
+++ components/VM_Arm/src/modules/plat/orinagx/fdt.c
@@ -26,11 +26,6 @@
 #define HSP_AON_PATH        "/bus@0/hsp@c150000"
 #define SERIAL_NODE_NAME    "serial"
 
-/* MGBE MAC address - locally administered (bit 1 of first octet = 1)
- * Format: string "xx:xx:xx:xx:xx:xx" for NVIDIA nvethernet driver
- */
-#define MGBE0_MAC_ADDR      "02:04:06:08:0a:0c"
-
 /* TCU mailbox parameters
  * TCU requires TWO mailboxes:
  *   - RX from HSP Top0 (shared mailbox 0)
@@ -204,148 +199,6 @@ static int fdt_generate_tcu_node(void *fdt)
 }
 
 /**
- * Add MGBE VM IRQ configuration node.
- *
- * NVIDIA's nvethernet driver requires nvidia,vm-irq-config phandle
- * pointing to a node with DMA channel to IRQ mapping.
- * Despite "VM" name, this is required even on bare metal.
- *
- * Creates /mgbe-vm-irq-config with one child (vm_irq0) for single
- * DMA channel configuration, and updates ethernet node's phandle reference.
- */
-static int fdt_add_mgbe_vm_irq_config(void *fdt)
-{
-    int err;
-    int root, eth_off, vm_cfg_off, vm_irq_off;
-    uint32_t vm_cfg_phandle;
-
-    /* Find ethernet node */
-    eth_off = fdt_path_offset(fdt, "/bus@0/ethernet@6800000");
-    if (eth_off < 0) {
-        ZF_LOGI("Ethernet node not found, skipping VM IRQ config");
-        return 0;  /* Not an error - ethernet might not be enabled */
-    }
-
-    /* Find root */
-    root = fdt_path_offset(fdt, "/");
-    if (root < 0) {
-        ZF_LOGE("Root node not found: %d", root);
-        return root;
-    }
-
-    /* Create /mgbe-vm-irq-config node */
-    vm_cfg_off = fdt_add_subnode(fdt, root, "mgbe-vm-irq-config");
-    if (vm_cfg_off < 0) {
-        if (vm_cfg_off == -FDT_ERR_EXISTS) {
-            vm_cfg_off = fdt_path_offset(fdt, "/mgbe-vm-irq-config");
-        } else {
-            ZF_LOGE("Failed to add mgbe-vm-irq-config: %d", vm_cfg_off);
-            return vm_cfg_off;
-        }
-    }
-
-    /* Assign phandle to vm-irq-config node */
-    vm_cfg_phandle = fdt_get_max_phandle(fdt) + 1;
-    err = fdt_setprop_u32(fdt, vm_cfg_off, "phandle", vm_cfg_phandle);
-    if (err) {
-        ZF_LOGE("Failed to set vm-irq-config phandle: %d", err);
-        return err;
-    }
-
-    /* Set nvidia,num-vm-irqs = 1 */
-    err = fdt_setprop_u32(fdt, vm_cfg_off, "nvidia,num-vm-irqs", 1);
-    if (err) {
-        ZF_LOGE("Failed to set num-vm-irqs: %d", err);
-        return err;
-    }
-
-    /* Create child node vm_irq0 */
-    vm_irq_off = fdt_add_subnode(fdt, vm_cfg_off, "vm_irq0");
-    if (vm_irq_off < 0) {
-        ZF_LOGE("Failed to add vm_irq0: %d", vm_irq_off);
-        return vm_irq_off;
-    }
-
-    /* Set vm_irq0 properties */
-    err = fdt_setprop_u32(fdt, vm_irq_off, "nvidia,vm-irq-id", 0);
-    if (err) return err;
-    err = fdt_setprop_u32(fdt, vm_irq_off, "nvidia,vm-num", 0);
-    if (err) return err;
-    err = fdt_setprop_u32(fdt, vm_irq_off, "nvidia,num-vm-channels", 1);
-    if (err) return err;
-
-    /* nvidia,vm-channels = <0> (single channel) */
-    uint32_t channels[1] = { cpu_to_fdt32(0) };
-    err = fdt_setprop(fdt, vm_irq_off, "nvidia,vm-channels", channels, sizeof(channels));
-    if (err) {
-        ZF_LOGE("Failed to set vm-channels: %d", err);
-        return err;
-    }
-
-    /* Re-lookup ethernet node (offset may have changed after adding nodes) */
-    eth_off = fdt_path_offset(fdt, "/bus@0/ethernet@6800000");
-    if (eth_off < 0) {
-        ZF_LOGE("Ethernet node disappeared: %d", eth_off);
-        return eth_off;
-    }
-
-    /* Update ethernet node's nvidia,vm-irq-config phandle */
-    err = fdt_setprop_u32(fdt, eth_off, "nvidia,vm-irq-config", vm_cfg_phandle);
-    if (err) {
-        ZF_LOGE("Failed to set ethernet vm-irq-config phandle: %d", err);
-        return err;
-    }
-
-    ZF_LOGI("Added mgbe-vm-irq-config (phandle 0x%x) with vm_irq0", vm_cfg_phandle);
-    return 0;
-}
-
-/**
- * Add MAC address to /chosen node for NVIDIA nvethernet driver.
- *
- * The NVIDIA nvethernet driver looks for MAC address at:
- *   /chosen/nvidia,ether-mac<N> where N is the mac-addr-idx from device node.
- *
- * For MGBE0 (mac-addr-idx=0), it looks for /chosen/nvidia,ether-mac0.
- * The property value is a string: "xx:xx:xx:xx:xx:xx"
- */
-static int fdt_add_mgbe_mac_address(void *fdt)
-{
-    int err;
-    int chosen_off;
-
-    /* Find or create /chosen node */
-    chosen_off = fdt_path_offset(fdt, "/chosen");
-    if (chosen_off < 0) {
-        /* Create /chosen if it doesn't exist */
-        int root = fdt_path_offset(fdt, "/");
-        if (root < 0) {
-            ZF_LOGE("Root node not found: %d", root);
-            return root;
-        }
-        chosen_off = fdt_add_subnode(fdt, root, "chosen");
-        if (chosen_off < 0) {
-            ZF_LOGE("Failed to add /chosen node: %d", chosen_off);
-            return chosen_off;
-        }
-        ZF_LOGI("Created /chosen node");
-    }
-
-    /* Add nvidia,ether-mac0 property */
-    err = fdt_setprop_string(fdt, chosen_off, "nvidia,ether-mac0", MGBE0_MAC_ADDR);
-    if (err) {
-        ZF_LOGE("Failed to set nvidia,ether-mac0: %d", err);
-        return err;
-    }
-
-    ZF_LOGI("Set /chosen/nvidia,ether-mac0 = %s", MGBE0_MAC_ADDR);
-    return 0;
-}
-
-/* Buffer size for DTB expansion - must match DTB_BUFFER_SIZE in main.c (320KB) */
-#define FDT_EXTRA_SPACE (128 * 1024)  /* 128KB extra for our additions */
-
-/**
  * Platform-specific DTB customization for Orin AGX.
  *
  * Called after CAmkES generates the base DTB from fdtgen but before fdt_pack().
@@ -354,16 +207,6 @@ static int fdt_add_mgbe_mac_address(void *fdt)
 int fdt_plat_customize(vm_t *vm, void *dtb_buf)
 {
     int err;
-    int current_size = fdt_totalsize(dtb_buf);
-    int new_size = current_size + FDT_EXTRA_SPACE;
-
-    /* Expand FDT to make room for new nodes/properties */
-    err = fdt_open_into(dtb_buf, dtb_buf, new_size);
-    if (err) {
-        ZF_LOGE("fdt_open_into failed: %d", err);
-        return err;
-    }
-    ZF_LOGI("Expanded FDT from %d to %d bytes", current_size, new_size);
 
     /* Set interrupt-parent at root for child interrupt routing */
     err = fdt_set_interrupt_parent(dtb_buf);
@@ -376,20 +219,6 @@ int fdt_plat_customize(vm_t *vm, void *dtb_buf)
     err = fdt_generate_tcu_node(dtb_buf);
     if (err) {
         ZF_LOGE("Cannot generate TCU serial node (%d)", err);
-        return -1;
-    }
-
-    /* Add MAC address to /chosen for MGBE0 (nvethernet) */
-    err = fdt_add_mgbe_mac_address(dtb_buf);
-    if (err) {
-        ZF_LOGE("Cannot add MGBE MAC address (%d)", err);
-        return -1;
-    }
-
-    /* Add MGBE VM IRQ configuration (required by nvethernet driver) */
-    err = fdt_add_mgbe_vm_irq_config(dtb_buf);
-    if (err) {
-        ZF_LOGE("Cannot add MGBE VM IRQ config (%d)", err);
         return -1;
     }
 
